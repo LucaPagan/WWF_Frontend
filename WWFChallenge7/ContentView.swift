6fca60449758d57676e89c98412ddbd2788bab04
Luca Pagano <149117354+LucaPagan@users.noreply.github.com>
2026-05-06 08:54:46 +0200

    Initial Commit

---
+++ WWFChallenge7/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WWFChallenge7
+//
+//  Created by Luca Pagano on 06/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WWFChallenge7/WWFChallenge7App.swift
@@ -0,0 +1,17 @@
+//
+//  WWFChallenge7App.swift
+//  WWFChallenge7
+//
+//  Created by Luca Pagano on 06/05/26.
+//
+
+import SwiftUI
+
+@main
+struct WWFChallenge7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
